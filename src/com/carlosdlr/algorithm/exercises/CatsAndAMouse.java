e745797119429d8250e4477fa2e229cc58fa269a
carlos andres de la rosa <kusanagi12002@gmail.com>
2020-05-03 18:41:54 +0200

    more hackerrank problems solutions

---
+++ src/com/carlosdlr/algorithm/exercises/CatsAndAMouse.java
@@ -0,0 +1,34 @@
+package com.carlosdlr.algorithm.exercises;
+
+/**
+ * Two cats and a mouse are at various positions on a line. You will be given their starting positions.
+ * Your task is to determine which cat will reach the mouse first, assuming the mouse doesn't move and the cats travel at equal speed.
+ * If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight.
+ * You are given  queries in the form of ,q , and  representing the respective positions for cats A and B, and for mouse C.
+ * Complete the function catAndMouse to return the appropriate answer to each query, which will be printed on a new line.
+ *
+ * If cat A catches the mouse first, print Cat A.
+ * If cat B catches the mouse first, print Cat B.
+ * If both cats reach the mouse at the same time, print Mouse C as the two cats fight and mouse escapes.
+ */
+public class CatsAndAMouse {
+
+    public static void main(String args []) {
+        System.out.println(catAndMouse(1,2,3));
+        System.out.println(catAndMouse(1,3,2));
+    }
+
+    // Complete the catAndMouse function below.
+    static String catAndMouse(int x, int y, int z) {
+        String result = "";
+
+        if(Math.abs(x - z) < Math.abs(y - z))
+            result = "Cat A";
+        else if(Math.abs(y - z) < Math.abs(x - z))
+            result = "Cat B";
+        else
+            result = "Mouse C";
+
+        return result;
+    }
+}
